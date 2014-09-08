6cb7472ad9993a1391cb1b275fcfe8db91e3a0f1
DoubelJ <DoubelJ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-08 08:19:10 +0000

    Add tag 1.4
    git-svn-id: https://plugins.svn.wordpress.org/show-dimensions-in-library/tags/1.4@984109 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ jajadi-show-dimensions.php
@@ -0,0 +1,86 @@
+<?php
+/*
+	Plugin Name: Show Dimensions in Library
+	Plugin URI: http://wordpress.org/extend/plugins/
+	Description: Show Dimensions in Media Library
+	Version: 1.4
+	Author: Janjaap van Dijk
+	Author URI: http://janjaapvandijk.nl/
+	Last Updated: 2014-04-09
+ 	License: GPLv2 or later
+ 	Text Domain: jajadi-show-dimensions
+	Domain Path: /languages/
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+function jajadi_show_dimensions_size_column_register($columns) {
+
+    $columns['dimensions'] = __('Dimensions', 'jajadi-show-dimensions');
+
+    return $columns;
+}
+
+
+function jajadi_show_dimensions_size_column_display($column_name, $post_id) {
+
+    if( 'dimensions' != $column_name || !wp_attachment_is_image($post_id))
+        return;
+
+    //list($url, $width, $height) = wp_get_attachment_image_src($post_id, 'full');
+	$metadata = wp_get_attachment_metadata( $post_id );
+	$width = $metadata['width'];
+	$height = $metadata['height'];
+
+    echo esc_html("{$width}&times;{$height}");
+}
+
+
+function jajadi_show_dimensions_load_textdomain() {
+	load_plugin_textdomain( 'jajadi-show-dimensions', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+
+// Register the column as sortable
+function jajadi_show_dimensions_size_column_register_sortable( $columns ) {
+	$columns['dimensions'] = 'dimensions';
+ 
+	return $columns;
+}
+
+
+function jajadi_show_dimensions_size_column_orderby( $query ) {
+    if( ! is_admin() )
+        return;
+ 
+    $orderby = $query->get( 'orderby');
+ 
+    if( 'dimensions' == $orderby ) {
+        $query->set('meta_key','_wp_attachment_metadata');
+        $query->set('orderby','meta_value');
+    }
+}
+
+
+// Hooks a function on to a specific action.
+add_action( 'plugins_loaded', 'jajadi_show_dimensions_load_textdomain');
+add_filter('manage_upload_columns', 'jajadi_show_dimensions_size_column_register');
+add_action('manage_media_custom_column', 'jajadi_show_dimensions_size_column_display', 10, 2);
+
+add_filter( 'manage_upload_sortable_columns', 'jajadi_show_dimensions_size_column_register_sortable' );
+add_action( 'pre_get_posts', 'jajadi_show_dimensions_size_column_orderby' );
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,73 @@
+=== Show Dimensions in Library ===
+Contributors: DoubelJ
+Donate link: http://janjaapvandijk.nl/
+Tags: images, media, dimensions, admin
+Requires at least: 3.0.1
+Tested up to: 4.0
+Stable tag: 1.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Show Dimensions in Media Library
+
+== Description ==
+
+Show Dimensions in Media Library 
+
+== Installation ==
+
+= Install from WordPress Dashboard =
+* Log into WordPress dashboard then click **Plugins** > **Add new** > 
+ * Then under the title "Install Plugins" click **Upload** > **choose the zip** > **Activate the plugin!**
+ * Or Search for **Show Dimensions in Library** and click **Install** > **Activate the plugin!**
+
+= Install from FTP =
+* Upload `/jajadi-show-dimensions/` to the `/wp-content/plugins/` directory
+* Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= I have a Question =
+You can find answers to your questions, suggest a feed, or just drop us a line at Support section.
+
+= Can you add my language to this plugin? =
+Yes we can. Go to [Transifex](https://www.transifex.com/) login or create an account and go to the project [`WP-Translations.org / Show Dimensions in Library - Wordpress Plugin`](https://www.transifex.com/projects/p/show-dimensions-in-library-wordpress-plugin/) and go translate. The next update we will add your translation to the plugin.
+Go to [WP-Translations Community](http://wp-translations.org/) for more information about translations.
+
+
+== Screenshots ==
+
+1. Dimensions in 3.9
+
+2. Dimensions in 4.0
+
+== Changelog ==
+
+= 1.4 =
+* Fix: The order of the column. It is not optimal now, but I'll make it better soon.
+
+= 1.3 =
+* Update: Make column sortable. Tnx to [Ben Huson](http://profiles.wordpress.org/husobj/)
+
+= 1.2 =
+* Add: German, French, Italian, Japanese, Korean, Russian language.
+
+= 1.1 =
+* Add: Dutch language.
+
+= 1.0 =
+* First relaese.
+
+== Upgrade Notice ==
+
+= 1.4 =
+The orderby was not good. I've build a work-around. I'll make it better soon.
+
+= 1.3 =
+Make Dimension column sortable
+
+= 1.2 =
+This version add German, French, Italian, Japanese, Korean, Russian language
+
+= 1.1 =
+This version add dutch language
